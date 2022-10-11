b41d4f1a430952e29a4bc9df63a0cfc338038ba2
Richard Murdoch <rmu@live.co.uk>
2022-10-11 16:29:34 +0100

    commit everything

--- pages/views.py
+++ pages/views.py
@@ -10,6 +10,32 @@ from sendgrid.helpers.mail import Mail
 from django.urls import resolve
 
 
+#class HomePageView(TemplateView):
+#    template_name = 'home.html'
+
+#class AboutPageView(TemplateView):
+#    template_name = 'index.html'
+
+def AboutPageView(request):
+    return render(request, "index.html", {'current_url': resolve(request.path_info).url_name})
+
+
+def ServicePageView(request):
+    return render(request, "services.html", {'current_url': resolve(request.path_info).url_name})
+
+
+
+def ContactPageView(request):
+    return render(request, "contact.html", {'current_url': resolve(request.path_info).url_name})
+
+def FeedbackPageView(request):
+    return render(request, "feedback.html", {'current_url': resolve(request.path_info).url_name})
+
+def GalleryPageView(request):
+    return render(request, "gallery.html", {'current_url': resolve(request.path_info).url_name})
+
+
+
 # TODO add expect to docker compose
 
 
@@ -29,7 +55,7 @@ def contactViewD(request):
             submit_message = form.cleaned_data['submit_message']
             try:
                 if settings.DEBUG:
-                    print(submit_name, submit_email, submit_number, submit_message, ['admin@example.com'])
+                    send_mail(submit_name, submit_email, submit_number, submit_message, ['admin@example.com'])
                 else:
                     #pass
                     def getsendmailapikey():
@@ -63,10 +89,36 @@ def contactViewD(request):
                 return HttpResponse('Invalid header found.')
             return redirect('demolition')
             #form='blah'
-    return render(request, "index.html", {'form': form, 'current_url': current_url})
+    return render(request, "demolition.html", {'form': form, 'current_url': current_url})
+
+def successViewD(request):
+    return render(request, "demolition.html", {'form': form})
+    return HttpResponse('Success! Thank you for your message. ')
+
+def data_flair(request):
+    return redirect('/static/services.html')
+
+
+def baseView(request):
+    return render(request, "base.html")
+
+def templateView(request):
+    return render(request, "appTemplate.html")
+
+#def faqsView(request):
+#    return render(request, "faqs.html")
+
+def faqsView(request):
+    return render(request, "faqs.html", {'current_url': resolve(request.path_info).url_name})
+
 
 
 #todo  add a success that says thanks we will be in touch
 
+# todo consolidate the repo and merge djpp into main if only one repo needed. update build
+# todo css for form
+# todo redirects for sensible access to flatter urls ceo like /demolition/ or /contact/
+# todo add emails
 # todo add logs
+# todo script that uses soup to gradb urls then a find replace to reploce them (needs to be in different dir from templates error for some reason)
 
