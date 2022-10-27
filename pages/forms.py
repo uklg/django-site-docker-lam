8b7a9547ac428ae66c20fbe89e67a18244692bfd
Richard Murdoch <rmu@live.co.uk>
2022-10-27 10:07:23 +0100

    add matching font colour and width

--- pages/forms.py
+++ pages/forms.py
@@ -4,16 +4,16 @@ from django.forms import EmailInput, TextInput
 class ContactForm(forms.Form):
     submit_name = forms.CharField(label='',widget=forms.TextInput(attrs={
 							'placeholder':'Your Name',
-							#'style': 'width: 300px;',
-							'class': 'input w-full bg-black border-b bg-opacity-0 text-white px-0 py-4 mb-4 tm-border-gold'}), required=False)
+							'style': 'width: 190px;',
+							'class': 'form-control'}), required=False)
     submit_email = forms.EmailField(label='',widget=forms.EmailInput(attrs={
                                                                 'placeholder':'Your Email',
-                                                                #'style': 'width: 300px;',
-                                                                'class': 'input w-full bg-black border-b bg-opacity-0 text-white px-0 py-4 mb-4 tm-border-gold'}), required=False)
+                                                                'style': 'width: 190px;',
+                                                                'class': 'form-control'}), required=False)
     submit_number = forms.CharField(label='',widget=forms.TextInput(attrs={
                                                                 'placeholder':'Your Number',
-                                                                #'style': 'width: 300px;',
-                                                                'class': 'input w-full bg-black border-b bg-opacity-0 text-white px-0 py-4 mb-4 tm-border-gold'}), required=False)
+                                                                'style': 'width: 190px;',
+                                                                'class': 'form-control'}), required=False)
 
 
 
